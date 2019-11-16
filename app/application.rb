370f6e24faaa6d440dc79b7eb888e1c8dc7d7fca
Will Drougas <wdrougas@gmail.com>
2019-11-16 09:45:17 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,18 +3,20 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
+    # num_1 = Kernel.rand(1..20)
+    # num_2 = Kernel.rand(1..20)
+    # num_3 = Kernel.rand(1..20)
  
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
+    # resp.write "#{num_1}\n"
+    # resp.write "#{num_2}\n"
+    # resp.write "#{num_3}\n"
+
+    resp.write Time.now.to_i
  
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+    if Time.now.hour < 12
+      resp.write "Good Morning!"
     else
-      resp.write "You Lose"
+      resp.write "Good Afternoon!"
     end
  
     resp.finish
